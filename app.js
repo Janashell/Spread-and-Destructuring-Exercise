d632323e6f9275a4f60ae122a766c4cc6bb71121
Janashell <janashell07@gmail.com>
2021-03-16 03:39:40 -0500

    Spread and Destructuring Exercise

---
+++ app.js
@@ -0,0 +1,51 @@
+// 1a
+const jPDinos = [
+    "Velociraptors", 
+    "Tyrannosaurus Rex",  
+    "Dilophosaurus"
+];
+
+// 1b
+function seeDinos(dino1, dino2, dino3) {
+    console.log(dino1);
+    console.log([dino2, dino3]);
+  
+}
+seeDinos(...jPDinos);
+
+// 2a
+const jPCharacters = {
+    alanGrant: "Sam Neill", 
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+};
+
+// 2b
+function seeCharacters({ellieSattler}){
+    console.log(ellieSattler);
+}
+seeCharacters(jPCharacters);
+
+// BONUS
+// 3a
+const jurrasicParkMovies = [{
+    one: "Jurassic Park", 
+    two: "The Lost World: Jurassic Park",  
+    three: "Jurassic Park III",
+
+    four: "Jurassic World",  
+    five: "Jurassic World: Fallen Kingdom"
+    }
+];
+
+// 3b
+function JPMovies({one, two, three,} {four, five}){
+ console.log(one);
+ console.log(two);
+ console.log(three);
+ console.log(four);
+ console.log(five);
+}
+JPMovies(jurrasicParkMovies);
+    
+
